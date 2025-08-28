f9d96a60640f225eaa6cfdb7f1c5a1728a471593
Sanchi285 <x@uni-weimar.de>
2025-08-28 17:59:22 +0200

    Soundtest Basic

---
+++ Assets/Scripts/WhistleSound.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class WhistleSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        // Call PlayWhistle every 1 second, starting immediately
+        InvokeRepeating("PlayWhistle", 0f, 1f);
+    }
+
+    void PlayWhistle()
+    {
+        audioSource.Play();
+    }
+}
